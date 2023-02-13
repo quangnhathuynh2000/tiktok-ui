18fedf06a1115f1c032899791b3a7700c18907b9
quangnhathuynh2000 <huynhnhatquang101220@gmail.com>
2023-02-13 16:11:23 +0700

    v4

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -6,9 +6,25 @@ import styles from './Header.module.scss'
 import images from '~/assets/images'
 import Button from '~/components/Button'
 import { Wrapper as PopperWrapper } from '~/components/Popper'
-import { faCircleXmark, faSpinner, faMagnifyingGlass, faSignIn } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faSpinner, faMagnifyingGlass, faSignIn, faEllipsisVertical, faEarthAsia, faCircleQuestion, faKeyboard } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Menu from '~/components/Popper/Menu';
 const cx = classNames.bind(styles)
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and help',
+        to: '/feedback'
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcuts',
+    }
+]
 function Header() {
     const [searchResult, setSearchResult] = useState([])
     useEffect(() => {
@@ -53,6 +69,11 @@ function Header() {
                     <Button text >Upload</Button>
                     <Button primary leftIcon={<FontAwesomeIcon icon={faSignIn} />} >Log in</Button>
 
+                    <Menu
+                        items={MENU_ITEMS}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} /> </button>
+                    </Menu>
                 </div>
             </div>
         </header >
